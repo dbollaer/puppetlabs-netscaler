66aeddd97946b673de5a4c88f0a9e61c6e0ea2c3
Danny <danny.bollaert@gmail.com>
2018-09-25 19:31:55 +0200

    fix add missing customserverid for loadbalancer
    https://developer-docs.citrix.com/projects/netscaler-nitro-api/en/10.5/configuration/basic/servicegroup_servicegroupmember_binding/servicegroup_servicegroupmember_binding/

--- lib/puppet/provider/netscaler_servicegroup_member/rest.rb
+++ lib/puppet/provider/netscaler_servicegroup_member/rest.rb
@@ -6,7 +6,7 @@ Puppet::Type.type(:netscaler_servicegroup_member).provide(:rest, {:parent => Pup
   end
 
   def self.instances
-    instances = []
+    instances     = []
     servicegroups = Puppet::Provider::Netscaler.call("/config/servicegroup")
     return [] if servicegroups.nil?
 
@@ -14,12 +14,13 @@ Puppet::Type.type(:netscaler_servicegroup_member).provide(:rest, {:parent => Pup
       binds = Puppet::Provider::Netscaler.call("/config/servicegroup_servicegroupmember_binding/#{servicegroup['servicegroupname']}") || []
       binds.each do |bind|
         instances << new({
-          :ensure    => :present,
-          :name      => "#{bind['servicegroupname']}/#{bind['servername']}:#{bind['port']}",
-          :weight    => bind['weight'],
-          :server_id => bind['serverid'],
-          :hash_id   => bind['hashid'],
-          :state     => bind['state'],
+          :ensure           => :present,
+          :name             => "#{bind['servicegroupname']}/#{bind['servername']}:#{bind['port']}",
+          :weight           => bind['weight'],
+          :server_id        => bind['serverid'],
+          :hash_id          => bind['hashid'],
+          :state            => bind['state'],
+          :custom_server_id => bind['customserverid'],
         })
       end
     end
@@ -32,8 +33,8 @@ Puppet::Type.type(:netscaler_servicegroup_member).provide(:rest, {:parent => Pup
   # Same as Puppet::Provider::NetscalerBinding
   def destroy
     toname, fromname_port = resource.name.split('/')
-    fromname, port = fromname_port.split(':')
-    result = Puppet::Provider::Netscaler.delete("/config/#{netscaler_api_type}/#{toname}",{'args'=>"servername:#{fromname},port:#{port}"})
+    fromname, port        = fromname_port.split(':')
+    result                = Puppet::Provider::Netscaler.delete("/config/#{netscaler_api_type}/#{toname}", {'args' => "servername:#{fromname},port:#{port}"})
     @property_hash.clear
 
     return result
@@ -52,7 +53,7 @@ Puppet::Type.type(:netscaler_servicegroup_member).provide(:rest, {:parent => Pup
 
   def per_provider_munge(message)
     message[:servicegroupname], server_port = message[:name].split('/')
-    message[:servername], message[:port] = server_port.split(':')
+    message[:servername], message[:port]    = server_port.split(':')
     message.delete(:name)
 
     message
